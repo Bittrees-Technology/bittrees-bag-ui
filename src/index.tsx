56153bfbe03387c4f88d08b4202404562d73926d
Brian Gershon <briangershon@users.noreply.github.com>
2023-07-01 10:52:55 -0700

    Upgrade for Wallet Connect v2

--- src/index.tsx
+++ src/index.tsx
@@ -32,6 +32,7 @@ const { chains, provider, webSocketProvider } = configureChains(
 
 const { connectors } = getDefaultWallets({
   appName: "Builders Advocacy Group",
+  projectId: "726b46a4ea5d9eaede5a7dc8a558c196",
   chains,
 });
 
