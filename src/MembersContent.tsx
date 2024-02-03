e3be6c37251cce7f4069514623d6f52576edafc3
Brian Gershon <briangershon@users.noreply.github.com>
2024-02-03 14:39:19 -0800

    Add "Forum" link to Member Services and reduce header whitespace. Move clubhouse link to the right.
    (and run prettier formatting)

--- src/MembersContent.tsx
+++ src/MembersContent.tsx
@@ -1,7 +1,7 @@
 import { useAccount } from "wagmi";
 import { useState, useEffect } from "react";
-import { useCookies } from 'react-cookie';
-import { hasActiveMembership, CONTRACT_ADDRESS } from './alchemy';
+import { useCookies } from "react-cookie";
+import { hasActiveMembership, CONTRACT_ADDRESS } from "./alchemy";
 
 /**
  * MembersContent React component.
@@ -21,20 +21,19 @@ export function MembersContent() {
   useEffect(() => {
     let token = cookies[CONTRACT_ADDRESS];
     if (!token) {
-
       console.log("isConnected: " + isConnected);
       if (!(address && isConnected)) {
         setHasValidMembership(false);
         return;
       }
-  
+
       hasActiveMembership(address)
         .then((hasActiveMembership) => {
           if (hasActiveMembership) {
             setCookie(CONTRACT_ADDRESS, address, {
-              path: '/'
+              path: "/",
             });
-          } 
+          }
           setHasValidMembership(hasActiveMembership);
         })
         .catch((err) => {
@@ -44,7 +43,6 @@ export function MembersContent() {
         .finally(() => {
           setLoading(false);
         });
-
     } else {
       setHasValidMembership(true);
       setLoading(false);
@@ -62,70 +60,104 @@ export function MembersContent() {
 
       {hasValidMembership && (
         <div className="m-4 mx-auto max-w-xl">
-          <h2 className="text-xl font-newtimesroman font-bold">Member Services</h2>
+          <h2 className="text-xl font-newtimesroman font-bold">
+            Member Services
+          </h2>
           <br />
           <div className="grid grid-cols-2 gap-6">
             <div className="text-left font-newtimesroman">
-            <ul className="max-w-md space-y-1 text-gray-400 list-disc list-outside ms-4 dark:text-gray-400 ">
+              <ul className="max-w-md space-y-1 text-gray-400 list-disc list-outside ms-4 dark:text-gray-400 ">
+                <li className="p-2">Gift Membership</li>
                 <li className="p-2">
-                  Gift Membership
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://etherscan.io/token/0x6e8c260cb878489c8066dd75536e5e9b5ca4c288#code"
+                  >
+                    Membership Contract
+                  </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://etherscan.io/token/0x6e8c260cb878489c8066dd75536e5e9b5ca4c288#code">Membership Contract</a>
+                    href="https://www.metaforo.io/g/bag/"
+                  >
+                    Forum
+                  </a>
                 </li>
                 <li className="p-2">Snapshot</li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://guild.xyz/bag">Telegram</a>
-                </li>
-                <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
-                    rel="noreferrer"
-                    href="https://www.voxels.com/play?coords=W@242W,572N,6U">Clubhouse</a>
+                    href="https://guild.xyz/bag"
+                  >
+                    Telegram
+                  </a>
                 </li>
               </ul>
-
             </div>
             <div className="text-left font-newtimesroman">
-              
-            <ul className="max-w-md space-y-1 text-gray-400 list-disc list-outside ms-4 dark:text-gray-400 ">
-            <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    href="/visionstatement">Vision Statement</a>
+              <ul className="max-w-md space-y-1 text-gray-400 list-disc list-outside ms-4 dark:text-gray-400 ">
+                <li className="p-2">
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    href="/visionstatement"
+                  >
+                    Vision Statement
+                  </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://docs.google.com/document/d/1BxcIkhfPnMf305VxGrEgEnfH2de_kFkL1iE43dI1WRo/edit?usp=sharing">
+                    href="https://docs.google.com/document/d/1BxcIkhfPnMf305VxGrEgEnfH2de_kFkL1iE43dI1WRo/edit?usp=sharing"
+                  >
                     Constitution [draft]
-                  </a> 
+                  </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    href="/billofrights">
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    href="/billofrights"
+                  >
                     Bill of Rights
-                  </a>  
+                  </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://docs.google.com/drawings/d/1s1c37-FNRjz3ZZP35UCWn-4viLZrAmQoMG1Q-IL7QQU/edit?usp=sharing">
+                    href="https://docs.google.com/drawings/d/1s1c37-FNRjz3ZZP35UCWn-4viLZrAmQoMG1Q-IL7QQU/edit?usp=sharing"
+                  >
                     Org Chart Diagram
                   </a>
                 </li>
                 <li className="p-2">
-                  <a className="text-black no-underline hover:underline hover:text-green-700" 
-                    target="_blank" 
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
                     rel="noreferrer"
-                    href="https://twitter.com/voxelsadvocacy">Twitter</a>
+                    href="https://twitter.com/voxelsadvocacy"
+                  >
+                    Twitter
+                  </a>
+                </li>
+                <li className="p-2">
+                  <a
+                    className="text-black no-underline hover:underline hover:text-green-700"
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.voxels.com/play?coords=W@242W,572N,6U"
+                  >
+                    Clubhouse
+                  </a>
                 </li>
               </ul>
             </div>
